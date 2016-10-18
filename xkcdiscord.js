1092586907aefca4ecba6a70b501042d04af0e69
Will Eccles <willeccles860+github@gmail.com>
2016-10-17 21:45:33 -0400

    Maybe fixed html characters.

--- xkcdiscord.js
+++ xkcdiscord.js
@@ -104,7 +104,7 @@ function getcomic(comicNumber, channel) {
 function htmldecode(s) {
 	if (/&#.+?;/.test(s))
 		return s.replace(/&#.+?;/g, (m) => {
-			return String.fromCharCode(m.replace(/&#/, "").replace(/;/, ""));
+			return String.fromCharCode(m.replace(/&#?/, "").replace(/;/, ""));
 		});
 	else return s;
 }
